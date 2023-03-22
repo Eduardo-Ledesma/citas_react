6ef4b8fbfa09ce87370019d58abcb2558f6b6dcf
Eduardo Ledesma <e.ledesma.higonet@gmail.com>
2023-03-22 11:02:24 -0300

    No fue mucho trabajo perro

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
     return (
         <h1 className="font-black text-5xl text-center lg:w-1/2 mx-auto lg:px-4">
             <span className="text-indigo-600">Veterinary </span>
-            Patients Monitoring
+            Patients Monitoring with Github
         </h1>
     )
 }
